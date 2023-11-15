e4608d8a05e5c5b1c1b834f67b7558931f3ae816
Karoline Ferreira <karoline.a.ferreira@hotmail.com>
2023-11-15 16:10:25 -0300

    Yelp Mvp

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,27 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
+  def new
+    @review = @restaurant.reviews.build
+  end
+
+  def create
+    @review = @restaurant.reviews.build(review_params)
+
+    if @review.save
+      redirect_to @restaurant, notice: "Review was successfully created"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+restaurants = [
+  { name: 'Galeto', address: '123 Street', category: 'chinese', phone_number: '301 303' },
+  { name: 'Narnia', address: 'Calle 13', category: 'french', phone_number: '141 303' },
+  { name: 'Frozen', address: 'Rua Banana 12', category: 'italian', phone_number: '859 303' },
+  { name: 'Maria Bonita', address: 'Av Suica 1140', category: 'japanese', phone_number: '301 444' },
+  { name: 'Mc Donals', address: 'Carretera 10', category: 'belgian', phone_number: '888 333' }
+]
+restaurants.each { |restaurant| Restaurant.create!(restaurant) }
